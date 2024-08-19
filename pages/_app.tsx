d5f8a49eceee6578b2f259545741786d0684400e
RoseGiftCode <aigiftson@protonmail.com>
2024-08-19 22:16:42 +0100

    Private work
    Its private

---
+++ pages/_app.tsx
@@ -0,0 +1,76 @@
+import { CssBaseline, GeistProvider } from '@geist-ui/core';
+import type { AppProps } from 'next/app';
+import NextHead from 'next/head';
+import GithubCorner from 'react-github-corner';
+import '../styles/globals.css';
+
+// Imports
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { JsonRpcProvider } from 'ethers'; // Import from ethers
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { arbitrum, bsc, gnosis, optimism, polygon, mainnet } from 'viem/chains';
+import { z } from 'zod';
+import { useIsMounted } from '../hooks';
+
+// Create JsonRpcProvider instances for each chain
+const rpcProvider = (chainId: number) => new JsonRpcProvider(`https://rpc.${chainId}.com`);
+
+const walletConnectProjectId = z
+  .string()
+  .parse(process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID);
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, bsc, gnosis],
+  [rpcProvider] // Use the custom provider function
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'rosewood',
+  projectId: "beac267da5e848a268c1c16282563a55", 
+  chains,
+  url: 'https://rosewoodng.com/', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const isMounted = useIsMounted();
+
+  if (!isMounted) return null;
+  return (
+    <>
+      <GithubCorner
+        href="https://github.com/dawsbot/drain"
+        size="140"
+        bannerColor="#e056fd"
+      />
+
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider coolMode chains={chains}>
+          <NextHead>
+            <title>Drain</title>
+            <meta
+              name="description"
+              content="Send all tokens from one wallet to another"
+            />
+            <link rel="icon" href="/favicon.ico" />
+          </NextHead>
+          <GeistProvider>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </GeistProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
+  );
+};
+
+export default App;
