b54a0501ad42ba246021f79edd422932b51de15d
RoseGiftCode <aigiftson@protonmail.com>
2024-08-21 12:53:17 +0100

    Update chain.ts

--- chain.ts
+++ chain.ts
@@ -1,282 +1,356 @@
 import { type Chain } from 'viem';
 
+function defineChain(chain) {
+  return {
+    formatters: undefined,
+    fees: undefined,
+    serializers: undefined,
+    ...chain,
+  };
+}
+
+const opStackChainConfig = {
+  contracts: {
+    multicall3: {
+      address: '0x...',
+      blockCreated: 0,
+    },
+  },
+};
+
+const zksyncChainConfig = {
+  contracts: {
+    multicall3: {
+      address: '0x...',
+      blockCreated: 0,
+    },
+  },
+};
+
+// Avalanche
+export const avalanche = /*#__PURE__*/ defineChain({
+  id: 43_114,
+  name: 'Avalanche',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Avalanche',
+    symbol: 'AVAX',
+  },
+  rpcUrls: {
+    default: { http: ['https://api.avax.network/ext/bc/C/rpc'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'SnowTrace',
+      url: 'https://snowtrace.io',
+      apiUrl: 'https://api.snowtrace.io',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 11907934,
+    },
+  },
+})
+
+// Arbitrum
+export const arbitrum = /*#__PURE__*/ defineChain({
+  id: 42_161,
+  name: 'Arbitrum One',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://arb1.arbitrum.io/rpc'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Arbiscan',
+      url: 'https://arbiscan.io',
+      apiUrl: 'https://api.arbiscan.io/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 7654707,
+    },
+  },
+})
+
+// BNB Smart Chain (BSC)
+export const bsc = /*#__PURE__*/ defineChain({
+  id: 56,
+  name: 'BNB Smart Chain',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'BNB',
+    symbol: 'BNB',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.ankr.com/bsc'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'BscScan',
+      url: 'https://bscscan.com',
+      apiUrl: 'https://api.bscscan.com/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 15921452,
+    },
+  },
+})
+
+// Base
+const baseSourceId = 1 // mainnet
+export const base = /*#__PURE__*/ defineChain({
+  ...opStackChainConfig,
+  id: 8453,
+  name: 'Base',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://mainnet.base.org'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Basescan',
+      url: 'https://basescan.org',
+      apiUrl: 'https://api.basescan.org/api',
+    },
+  },
+  contracts: {
+    ...opStackChainConfig.contracts,
+    l2OutputOracle: {
+      [baseSourceId]: {
+        address: '0x56315b90c40730925ec5485cf004d835058518A0',
+      },
+    },
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 5022,
+    },
+    portal: {
+      [baseSourceId]: {
+        address: '0x49048044D57e1C92A77f79988d21Fa8fAF74E97e',
+        blockCreated: 17482143,
+      },
+    },
+    l1StandardBridge: {
+      [baseSourceId]: {
+        address: '0x3154Cf16ccdb4C6d922629664174b904d80F2C35',
+        blockCreated: 17482143,
+      },
+    },
+  },
+  sourceId: baseSourceId,
+})
+
+// Polygon
+export const polygon = /*#__PURE__*/ defineChain({
+  id: 137,
+  name: 'Polygon',
+  nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://polygon-rpc.com'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'PolygonScan',
+      url: 'https://polygonscan.com',
+      apiUrl: 'https://api.polygonscan.com/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 25770160,
+    },
+  },
+})
+
+// ZKsync Era
+export const zksync = /*#__PURE__*/ defineChain({
+  ...zksyncChainConfig,
+  id: 324,
+  name: 'ZKsync Era',
+  network: 'zksync-era',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://mainnet.era.zksync.io'],
+      webSocket: ['wss://mainnet.era.zksync.io/ws'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Etherscan',
+      url: 'https://era.zksync.network/',
+      apiUrl: 'https://api-era.zksync.network/api',
+    },
+    native: {
+      name: 'ZKsync Explorer',
+      url: 'https://explorer.zksync.io/',
+      apiUrl: 'https://block-explorer-api.mainnet.zksync.io/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xF9cda624FBC7e059355ce98a31693d299FACd963',
+    },
+  },
+})
+
+// Nexilix Smart Chain
+export const nexilix = /*#__PURE__*/ defineChain({
+  id: 240,
+  name: 'Nexilix Smart Chain',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Nexilix',
+    symbol: 'NEXILIX',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpcurl.pos.nexilix.com'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'NexilixScan',
+      url: 'https://scan.nexilix.com',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0x58381c8e2BF9d0C2C4259cA14BdA9Afe02831244',
+      blockCreated: 74448,
+    },
+  },
+})
+
+// Gnosis
+export const gnosis = /*#__PURE__*/ defineChain({
+  id: 100,
+  name: 'Gnosis',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Gnosis',
+    symbol: 'xDAI',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.gnosischain.com'],
+      webSocket: ['wss://rpc.gnosischain.com/wss'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Gnosisscan',
+      url: 'https://gnosisscan.io',
+      apiUrl: 'https://api.gnosisscan.io/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 21022491,
+    },
+  },
+})
+
+// Ethereum Classic
+export const classic = /*#__PURE__*/ defineChain({
+  id: 61,
+  name: 'Ethereum Classic',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'ETC',
+    symbol: 'ETC',
+  },
+  rpcUrls: {
+    default: { http: ['https://etc.rivet.link'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Blockscout',
+      url: 'https://blockscout.com/etc/mainnet',
+    },
+  },
+})
+
 // Ethereum Mainnet
-export const mainnet: Chain = {
+export const mainnet = /*#__PURE__*/ defineChain({
   id: 1,
   name: 'Ethereum',
   nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
   rpcUrls: {
-    default: { http: ['https://cloudflare-eth.com'] },
+    default: {
+      http: ['https://cloudflare-eth.com'],
+    },
   },
   blockExplorers: {
-    default: { name: 'Etherscan', url: 'https://etherscan.io' },
+    default: {
+      name: 'Etherscan',
+      url: 'https://etherscan.io',
+      apiUrl: 'https://api.etherscan.io/api',
+    },
   },
   contracts: {
-    ensRegistry: {
-      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-    },
-    ensUniversalResolver: {
-      address: '0xE4Acdd618deED4e6d2f03b9bf62dc6118FC9A4da',
-      blockCreated: 16773775,
-    },
     multicall3: {
       address: '0xca11bde05977b3631167028862be2a173976ca11',
       blockCreated: 14353601,
     },
   },
-} as const;
-
-// Polygon (MATIC)
-export const polygon: Chain = {
-  id: 137,
-  name: 'Polygon',
-  nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://polygon-rpc.com'] },
-  },
-  blockExplorers: {
-    default: { name: 'Polygonscan', url: 'https://polygonscan.com' },
-  },
-  contracts: {
-    ensRegistry: {
-      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e', // Same as Ethereum
-    },
-    ensUniversalResolver: {
-      address: '0xE4Acdd618deED4e6d2f03b9bf62dc6118FC9A4da', // Same as Ethereum
-    },
-    multicall3: {
-      address: '0x6e1c27f04f7a5834c65de60377fbd8f0fbb0a243',
-    },
-  },
-} as const;
-
-// Optimism
-export const optimism: Chain = {
-  id: 10,
-  name: 'Optimism',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://mainnet.optimism.io'] },
-  },
-  blockExplorers: {
-    default: { name: 'Optimistic Etherscan', url: 'https://optimistic.etherscan.io' },
-  },
-  contracts: {
-    ensRegistry: {
-      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e', // Same as Ethereum
-    },
-    ensUniversalResolver: {
-      address: '0xE4Acdd618deED4e6d2f03b9bf62dc6118FC9A4da', // Same as Ethereum
-    },
-    multicall3: {
-      address: '0x72e9c7d5961e9d74a054cbca9253cf4ef15f4748',
-    },
-  },
-} as const;
-
-// Arbitrum
-export const arbitrum: Chain = {
-  id: 42161,
-  name: 'Arbitrum',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://arb1.arbitrum.io/rpc'] },
-  },
-  blockExplorers: {
-    default: { name: 'Arbiscan', url: 'https://arbiscan.io' },
-  },
-  contracts: {
-    ensRegistry: {
-      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e', // Same as Ethereum
-    },
-    ensUniversalResolver: {
-      address: '0xE4Acdd618deED4e6d2f03b9bf62dc6118FC9A4da', // Same as Ethereum
-    },
-    multicall3: {
-      address: '0x6d2e6a2d49a77d54d486089b5f2dc034891e9d60',
-    },
-  },
-} as const;
-
-// Binance Smart Chain (BSC)
-export const bsc: Chain = {
-  id: 56,
-  name: 'Binance Smart Chain',
-  nativeCurrency: { name: 'Binance Coin', symbol: 'BNB', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://bsc-dataseed.binance.org/'] },
-  },
-  blockExplorers: {
-    default: { name: 'BscScan', url: 'https://bscscan.com' },
-  },
-  contracts: {
-    ensRegistry: {
-      address: '0x0000000000000000000000000000000000000000', // Not applicable
-    },
-    ensUniversalResolver: {
-      address: '0x0000000000000000000000000000000000000000', // Not applicable
-    },
-    multicall3: {
-      address: '0x0e8b792c0a6d3e7a6452b08e47304b0d4d58331a',
-    },
-  },
-} as const;
-
-// Gnosis
-export const gnosis: Chain = {
-  id: 100,
-  name: 'Gnosis',
-  nativeCurrency: { name: 'xDAI', symbol: 'xDAI', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://rpc.gnosis.gateway.fm'] },
-  },
-  blockExplorers: {
-    default: { name: 'Gnoscan', url: 'https://gnoscan.io' },
-  },
-  contracts: {
-    ensRegistry: {
-      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e', // May use Ethereum's ENS
-    },
-    ensUniversalResolver: {
-      address: '0xE4Acdd618deED4e6d2f03b9bf62dc6118FC9A4da', // Confirm with Gnosis
-    },
-    multicall3: {
-      address: '0x3e7e2f021f18d1f11c4b1d8311cfb5d19d7cf766',
-    },
-  },
-} as const;
-
-// zkSync In-Memory Node
-export const zkSyncInMemoryNode: Chain = {
-  id: 0, // Specific to zkSync In-Memory Node, not standardized
-  name: 'zkSync In-Memory Node',
-  nativeCurrency: { name: 'zkSync Ether', symbol: 'zkETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://zksync2-testnet.zksync.dev'] },
-  },
-  blockExplorers: {
-    default: { name: 'zkSync Explorer', url: 'https://explorer.zksync.io' },
-  },
-  contracts: {
-    ensRegistry: {
-      address: '0x0000000000000000000000000000000000000000', // Not applicable
-    },
-    ensUniversalResolver: {
-      address: '0x0000000000000000000000000000000000000000', // Not applicable
-    },
-    multicall3: {
-      address: '0x0000000000000000000000000000000000000000', // Specific to zkSync
-    },
-  },
-} as const;
-
-// Nexilix
-export const nexilix: Chain = {
-  id: 0, // Specific to Nexilix, not standardized
-  name: 'Nexilix',
-  nativeCurrency: { name: 'Nexilix Token', symbol: 'NEXI', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://rpc.nexilix.io'] },
-  },
-  blockExplorers: {
-    default: { name: 'Nexilix Explorer', url: 'https://explorer.nexilix.io' },
-  },
-  contracts: {
-    ensRegistry: {
-      address: '0x0000000000000000000000000000000000000000', // Check Nexilix resources
-    },
-    ensUniversalResolver: {
-      address: '0x0000000000000000000000000000000000000000', // Check Nexilix resources
-    },
-    multicall3: {
-      address: '0x0000000000000000000000000000000000000000', // Check Nexilix resources
-    },
-  },
-} as const;
-
-// zkSync
-export const zkSync: Chain = {
-  id: 324,
-  name: 'zkSync',
-  nativeCurrency: { name: 'zkSync Ether', symbol: 'zkETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://zksync2-mainnet.zksync.io'] },
-  },
-  blockExplorers: {
-    default: { name: 'zkSync Explorer', url: 'https://explorer.zksync.io' },
-  },
-  contracts: {
-    ensRegistry: {
-      address: '0x0000000000000000000000000000000000000000', // zkSync usually doesn’t use ENS
-    },
-    ensUniversalResolver: {
-      address: '0x0000000000000000000000000000000000000000', // Confirm with zkSync
-    },
-    multicall3: {
-      address: '0x6f4e5b1f2b9b6a19c5b7fa63bb8a7cfd567cf061',
-    },
-  },
-} as const;
-
-// Ethereum Classic (Classic)
-export const classic: Chain = {
-  id: 61,
-  name: 'Ethereum Classic',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://www.ethercluster.com/etc'] },
-  },
-  blockExplorers: {
-    default: { name: 'Ether Classic Explorer', url: 'https://blockscout.com/etc/mainnet' },
-  },
-  contracts: {
-    ensRegistry: {
-      address: '0x0000000000000000000000000000000000000000', // Not applicable
-    },
-    ensUniversalResolver: {
-      address: '0x0000000000000000000000000000000000000000', // Not applicable
-    },
-    multicall3: {
-      address: '0x3e7e2f021f18d1f11c4b1d8311cfb5d19d7cf766',
-    },
-  },
-} as const;
+})
 
 // Dogechain
-export const dogechain: Chain = {
-  id: 0, // Specific to Dogechain, not standardized
+export const dogechain = /*#__PURE__*/ defineChain({
+  id: 2000,
   name: 'Dogechain',
-  nativeCurrency: { name: 'Dogecoin', symbol: 'DOGE', decimals: 8 },
+  nativeCurrency: {
+    decimals: 18,
+    name: 'DOGE',
+    symbol: 'DOGE',
+  },
   rpcUrls: {
-    default: { http: ['https://rpc.dogechain.dog'] },
+    default: { http: ['https://rpc01.dogechain.dog'] },
   },
   blockExplorers: {
-    default: { name: 'Dogechain Explorer', url: 'https://explorer.dogechain.dog' },
+    default: {
+      name: 'Dogechain Explorer',
+      url: 'https://explorer.dogechain.dog',
+    },
   },
   contracts: {
-    ensRegistry: {
-      address: '0x0000000000000000000000000000000000000000', // Not applicable
-    },
-    ensUniversalResolver: {
-      address: '0x0000000000000000000000000000000000000000', // Not applicable
-    },
     multicall3: {
-      address: '0x0000000000000000000000000000000000000000', // Specific to Dogechain
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 344484,
     },
   },
-} as const;
+})
 
-// Export all chains as a readonly tuple
-export const chains = [
-  mainnet,
-  polygon,
-  optimism,
+// Export chains
+export const chains = {
+  avalanche,
   arbitrum,
   bsc,
-  gnosis,
-  zkSyncInMemoryNode,
+  base,
+  polygon,
+  zksync,
   nexilix,
-  zkSync,
+  gnosis,
   classic,
+  mainnet,
   dogechain,
-] as const;
+}
