ea56c5e2f2fa6e24a82d1dc3a8412aaa24be8381
RoseGiftCode <aigiftson@protonmail.com>
2024-08-23 09:44:30 +0100

    Update [evmAddress].ts

--- pages/api/chain-info/[chainId]/[evmAddress].ts
+++ pages/api/chain-info/[chainId]/[evmAddress].ts
@@ -2,6 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
 
 const ALCHEMY_API_KEY = z.string().parse(process.env.ALCHEMY_API_KEY);
+console.log('WalletConnect Project ID:', process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID);
+
 
 type ChainName =
   | 'eth-mainnet'
